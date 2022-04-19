fd240f08b549cc3fa894f00eca0ce524fca2f5d3
Matt Knop <mknop@redhat.com>
2022-04-19 09:37:08 -0600

    Configuring for ConfigMap usage.

--- controllers/cloud.redhat.com/config.go
+++ controllers/cloud.redhat.com/config.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"strings"
 
+	configmap "k8s.io/client-go/listers/core/v1"
+
 	clowder "github.com/RedHatInsights/clowder/apis/cloud.redhat.com/v1alpha1"
 	frontend "github.com/RedHatInsights/frontend-operator/api/v1alpha1"
 	core "k8s.io/api/core/v1"
@@ -31,20 +33,23 @@ type PoolConfig struct {
 }
 
 func getConfig() OperatorConfig {
-	configMapPath := "config/environment_config.yaml"
-	configMapFileName := configMapPath[strings.LastIndex(configMapPath, "/")+1:]
+	// configMapPath := "config/environment_config.yaml"
+	// configMapFileName := configMapPath[strings.LastIndex(configMapPath, "/")+1:]
 
-	configMapData, err := ioutil.ReadFile(configMapPath)
-	if err != nil {
-		fmt.Printf("%s not found\n", configMapFileName)
-		return OperatorConfig{}
-	} else {
-		fmt.Printf("Loading config from: %s\n", configMapFileName)
-	}
+	// configMapData, err := ioutil.ReadFile(configMapPath)
+	// if err != nil {
+	// 	fmt.Printf("%s not found\n", configMapFileName)
+	// 	return OperatorConfig{}
+	// } else {
+	// 	fmt.Printf("Loading config from: %s\n", configMapFileName)
+	// }
+
+	cm, err := configmap.Get("default")
 
 	configMap := ConfigMap{}
 	operatorConfig := OperatorConfig{}
-	err = yaml.Unmarshal(configMapData, &configMap)
+	err = yaml.Unmarshal(cm, &configMap)
+	//err = yaml.Unmarshal(configMapData, &configMap)
 	if err != nil {
 		fmt.Printf("Couldn't parse json:\n" + err.Error())
 		return OperatorConfig{}
